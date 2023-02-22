5c270ee26eb7bf329fa8397330f0a677944a9e8e
Hiranya-27 <125944628+Hiranya-27@users.noreply.github.com>
2023-02-22 18:18:18 +0530

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,13 @@
+
+package com.example.demo;
+
+public class Message {
+   private String mes;
+   public Message(String mes)
+   {
+	   this.mes=mes;
+   }
+   public String getMes() {
+	   return mes;
+   }
+}
